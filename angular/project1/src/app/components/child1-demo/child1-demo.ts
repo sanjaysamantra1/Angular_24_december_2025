8b00e479c4fc86aa6cd4d824e813950e086945fa
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-01-30 07:38:12 +0530

    lifecycle

--- angular/project1/src/app/components/child1-demo/child1-demo.ts
+++ angular/project1/src/app/components/child1-demo/child1-demo.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-child1-demo',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './child1-demo.html',
   styleUrl: './child1-demo.css',
-  inputs: ['a','userName']
+  inputs: ['a', 'userName', 'user'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Child1Demo {
   a: any;
-  userName:any;
+  userName: any;
+  user: any;
+
+  constructor(private cdr: ChangeDetectorRef) {
+    console.log('child-1 constructor')
+  }
+  ngOnChanges(myChanges: any) {
+    console.log('child-1 ngOnChanges', myChanges)
+  }
+  ngOnInit() {
+    console.log('child-1 ngOnInit');
+  }
+  ngDoCheck() { // when parent's object data changes, onchanges is not called
+    // but docheck is called
+    console.log('child-1 ngDoCheck');
+    this.cdr.markForCheck();
+  }
 }
--- angular/project1/src/app/components/parent-demo/parent-demo.ts
+++ angular/project1/src/app/components/parent-demo/parent-demo.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { Child1Demo } from '../child1-demo/child1-demo';
 import { Child2Demo } from '../child2-demo/child2-demo';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-parent-demo',
   imports: [
+    CommonModule,
+    FormsModule,
     Child1Demo,
     Child2Demo
   ],
@@ -12,10 +16,39 @@ import { Child2Demo } from '../child2-demo/child2-demo';
   styleUrl: './parent-demo.css',
 })
 export class ParentDemo {
-  a = 10;
-  addressParent: string = '';
+  user = { name: 'sanjay', role: 'trainer', address: 'bangalore' };
 
+  a: number;
+  addressParent: string = '';
   receiveDataFromChild(addressFromChild: any) {
     this.addressParent = addressFromChild;
   }
+  constructor() {
+    this.a = 100;
+    console.log("Parent constructor")
+  }
+  ngOnChanges() {
+    console.log('Parent ngOnChanges');
+  }
+  ngOnInit() {
+    console.log('Parent ngOnInit');
+  }
+  ngDoCheck() {
+    console.log('Parent ngDoCheck');
+  }
+  // ngAfterContentInit() {
+  //   console.log('Parent ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('Parent ngAfterContentChecked')
+  // }
+  // ngAfterViewInit() {
+  //   console.log('Parent ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('Parent ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('Parent ngOnDestory');
+  // }
 }
